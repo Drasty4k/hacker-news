129c1e282f72af85a0df81dfd67eafdf6727ce14
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-12-09 16:24:08 +0100

    fetched all the news ID's and some styling changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
-import styles from "../styles/Home.module.scss";
+import useSWR from "swr";
+import NewsItem from "../components/news-item/news-item";
+import styles from "./index.module.scss";
+
+export const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  const { data: newsIds, error: newsIdsError } = useSWR<number[]>(
+    'https://hacker-news.firebaseio.com/v0/topstories.json?limitToFirst=10&orderBy="$key"',
+    fetcher
+  );
+
+  if (newsIdsError) {
+    return <p>error</p>;
+  }
+
+  if (!newsIds) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <ul className={styles.newsList}>
+        {newsIds?.map((id) => (
+          <NewsItem key={id} id={id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
