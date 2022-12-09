c57723b0656081adc663a2cc09627308b0276ca8
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-12-09 19:34:11 +0100

    Implemented the sort functionality by moving up one component the newsItem fetching to have acces to all the elements in an array. Sort the newsItems by pressing the toggle button.

--- components/UI/icons.tsx
+++ components/UI/icons.tsx
@@ -34,3 +34,32 @@ export const ArrowRightIcon: React.FC<IconProps> = ({
     </svg>
   );
 };
+
+export const ArrowUpDownIcon: React.FC<IconProps> = ({
+  className,
+  fill = "none",
+  height = 22,
+  width = 22,
+  strokeWidth = "1.5",
+  strokeColor = "#fff",
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={fill}
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke={strokeColor}
+      width={width}
+      height={height}
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+      />
+    </svg>
+  );
+};
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,55 @@
+import { useEffect, useMemo, useState } from "react";
 import useSWR from "swr";
 import NewsItem from "../components/news-item/news-item";
+import { ArrowUpDownIcon } from "../components/UI/icons";
 import styles from "./index.module.scss";
 
 export const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
+const fetchNewsItem = async (newsId: number) => {
+  const response = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${newsId}.json`
+  );
+  const json = await response.json();
+  return json;
+};
+
+type ResponseData = {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  parts: number[];
+  score: number;
+  text: string;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+};
+
 export default function Home() {
   const { data: newsIds, error: newsIdsError } = useSWR<number[]>(
     'https://hacker-news.firebaseio.com/v0/topstories.json?limitToFirst=10&orderBy="$key"',
-    fetcher
+    { fetcher, revalidateOnFocus: false }
   );
+  const [newsItems, setNewsItems] = useState<ResponseData[]>([]);
+  const [order, setOrder] = useState<"Asceding" | "Descending">("Asceding");
+  const [pressed, setPressed] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (newsIds?.length! > 0) {
+      newsIds?.map(async (id) => {
+        const newItem = (await fetchNewsItem(id)) as ResponseData;
+        setNewsItems((prev) => [...prev, newItem]);
+      });
+    }
+  }, [newsIds]);
+
+  const toggleSort = () => {
+    setPressed((prev) => !prev);
+    setOrder((prev) => (prev === "Asceding" ? "Descending" : "Asceding"));
+  };
 
   if (newsIdsError) {
     return <p>error</p>;
@@ -18,11 +59,28 @@ export default function Home() {
     return <p>Loading...</p>;
   }
 
+  const sortedNewsItems = newsItems.sort((a, b) => {
+    return order === "Asceding" ? b.score - a.score : a.score - b.score;
+  });
+
   return (
     <div className={styles.container}>
+      <button
+        onClick={toggleSort}
+        className={`${styles.toggleSortBtn} ${pressed ? styles.rotate : ""}`}
+      >
+        <ArrowUpDownIcon strokeColor="#1fb65e" width={30} height={30} />
+      </button>
       <ul className={styles.newsList}>
-        {newsIds?.map((id) => (
-          <NewsItem key={id} id={id} />
+        {sortedNewsItems.map(({ id, time, by, title, score, url }) => (
+          <NewsItem
+            key={id}
+            time={time}
+            by={by}
+            title={title}
+            score={score}
+            url={url}
+          />
         ))}
       </ul>
     </div>
