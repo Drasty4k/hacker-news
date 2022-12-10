ac954e4e3df095702dc8f692c930a5dd7c0f2606
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-12-10 11:57:51 +0100

    added the blob images and exported them as an object

---
+++ public/assets/index.ts
@@ -0,0 +1,25 @@
+import blob1 from "./blob-1.svg";
+import blob2 from "./blob-2.svg";
+import blob3 from "./blob-3.svg";
+import blob4 from "./blob-4.svg";
+import blob5 from "./blob-5.svg";
+import blob6 from "./blob-6.svg";
+import blob7 from "./blob-7.svg";
+import blob8 from "./blob-8.svg";
+import blob9 from "./blob-9.svg";
+import blob10 from "./blob-10.svg";
+
+const blobImages: { [key: string]: string } = {
+  blob1,
+  blob2,
+  blob3,
+  blob4,
+  blob5,
+  blob6,
+  blob7,
+  blob8,
+  blob9,
+  blob10,
+};
+
+export default blobImages;
